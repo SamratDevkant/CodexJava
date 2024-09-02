b10cfca2a5cfdb3f918a3da2d02d483a808751ea
devkant samrat <devkantsamratashok@gmail.com>
2024-09-02 23:23:43 +0530

    Day 5 Assignment done

---
+++ day 5/Program3.java
@@ -0,0 +1,23 @@
+class Program3 {
+
+	public static void main(String[]args){
+	
+	char ch = 'x';
+	char ch1 = 'b';
+	
+	if (ch=='a'||ch== 'e'||ch=='i'||ch=='o'||ch=='u'){	
+
+		System.out.println(ch+ " Is ovel");
+	}else{
+		System.out.println(ch+ " Not ovel");
+
+	}
+	
+	
+
+}
+	
+
+
+
+}
