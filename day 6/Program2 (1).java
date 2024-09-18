6b6b8bf85f6a38db75cb427acac0f4f54efb346c
devkant samrat <devkantsamratashok@gmail.com>
2024-09-18 12:52:45 +0530

    Assignment done

---
+++ day 6/Program2 (1).java
@@ -0,0 +1,21 @@
+class Program2{
+	public static void main(String[]args){
+
+		int a=20;
+		int b=30;
+		int c=40;
+		if (a<b && a<c){
+			System.out.println(a+" is a smallest Number");
+		}else if (b<a && b<c){
+			System.out.println(b+" is a smallest number");
+		}
+		else{ 
+			System.out.println(c+" is a smallest number");
+		
+}
+	
+}
+	
+
+
+}
